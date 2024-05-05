c61022d2b7340cc45629d75eb3a482b80f2f550d
caleb swenson <calebswenson@calebs-MacBook-Pro.local>
2024-05-04 22:00:08 -0700

    remove author overlap

--- app/models.py
+++ app/models.py
@@ -131,6 +131,7 @@ class Dream(db.Model):
             'keywords': list(self.keywords),
             'author': self.author.to_dict(),
             'likes': self.likes,
+            'who_liked': [user.to_dict() for user in self.who_liked],
             'interpretations': [
                 interpretation.to_dict() for interpretation in self.interpretations
             ],
